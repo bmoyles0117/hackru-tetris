bb8b649afd19c57105bd0a82d8894f2712565a7c
Bryan Moyles <bmoy117@gmail.com>
2014-04-13 11:06:18 -0400

    Cleaning up the frontend a bit

--- public/js/app.js
+++ public/js/app.js
@@ -1,135 +1,85 @@
 
 tetrisApp = angular.module("tetrisApp" , [])
   // .config(function($interpolateProvider) {
-  // 	  $interpolateProvider.startSymbol('[[');
-  // 	  $interpolateProvider.endSymbol(']]');
+  //   $interpolateProvider.startSymbol('[[');
+  //   $interpolateProvider.endSymbol(']]');
   // })
 tetrisApp.directive('gameStarter', function() {
   return function($scope, element, attrs) {
-    console.log(element);
-
     element.bind('click', function() {
       $.post('/start');
       
       return false;
     });
   }
-})
+});
 tetrisApp.controller("HomepageCtrl",["$scope", function($scope) {
-    $scope.game_over = true;
-    $scope.game_over1 = true;
-    $scope.game_over2 = true;
+  $scope.game_over = true;
+  $scope.loading = true;
+  $scope.teams = 2;
 
-    $scope.loading = true;
-    $scope.teams = 1;
+  $scope.colors = {
+    73 : "blue",
+    74 : "green",
+    76 : "yellow",
+    79 : "red",
+    83 : "cyan",
+    84 : "black",
+    90 : "gray"
+  };
 
+  var pusher = new Pusher('c2388f10a4afc865f3a5');
+  var channel = pusher.subscribe('my_channel');
 
-    $scope.colors = {
-          73 : "blue",
-          74 : "green",
-          76 : "yellow",
-          79 : "red",
-          83 : "cyan",
-          84 : "black",
-          90 : "gray"
-       };
+  channel.bind('my_event', function(data) {
+    $scope.game_over = data.game_over;
 
+    if(data.game_over) {
+      alert('GAME OVER');
+      return
+    }
 
-    $scope.pusher = new Pusher('c2388f10a4afc865f3a5');
-    $scope.channel = $scope.pusher.subscribe('my_channel');
+    var canvas = document.getElementById(data.board_key == "+17327305402" ? 'player1_board' : 'player2_board');
+    var context = canvas.getContext('2d');
 
+    canvas.width = canvas.width;
 
-    $scope.channel.bind('my_event', function(data) {
-    	$scope.gameData = data.cells;
-      $scope.game_over = data.game_over;
-      // $scope.gameNextPiece = data.next_piece;
+    for(var i = 0; i < data.cells.length; i++){
+      for(var j = 0; j < data.cells[i].length; j++ ) {
+        var rows = data.cells.length,
+            cols = data.cells[i].length,
+            width = canvas.width/cols,
+            height = canvas.height/rows;
 
-      if(data.game_over) {
-        alert('GAME OVER');
-        return
+        if (data.cells[i][j] != 0) {
+          context.fillStyle = $scope.colors[data.cells[i][j]];
+          context.fillRect(width*j, height*i, width, height);
+          context.strokeStyle = "#f9f9f9";
+          context.strokeRect(width*j, height*i, width, height);
+        }
       }
-
-
-      $scope.drawPieces = function () {
-
-        if( data.board_key == "+17327305402"  ){
-          var canvas = document.getElementById('myCanvas');
-        }else {
-          var canvas = document.getElementById('myCanvas3');
-        }
-
-        var context = canvas.getContext('2d');
-
-        canvas.width = canvas.width;
-
-        for(i=0; i<22; i++){
-        	for(j=0; j<12; j++ ) {
-
-            if ($scope.gameData[i][j] != 0){
-    					context.fillStyle=$scope.colors[$scope.gameData[i][j]];
-              context.fillRect( ((canvas.width/12)*j) ,((canvas.height/22)*i),canvas.width/12, canvas.height/22);
-               context.strokeStyle="#f9f9f9";
-              context.strokeRect( ((canvas.width/12)*j) ,((canvas.height/22)*i),canvas.width/12, canvas.height/22);
-
-    				}
-
-
-
-
-
-        	}
-
-        }
-        if(data.next_piece){
-          piece_size = (data.next_piece.shape[0]).length;
-        
-        if( data.board_key == "+17327305402"  ){
-          var canvas = document.getElementById('myCanvas2');
-        }else {
-          var canvas = document.getElementById('myCanvas4');
-        }
-          var context = canvas.getContext('2d');
-
-        canvas.width = canvas.width;
-
-        console.log(piece_size);
-
-        for(i=0; i<piece_size; i++){
-          for(j=0; j<piece_size; j++ ) {
-
-            if (data.next_piece.shape[i][j] != 0){
-              context.fillStyle=$scope.colors[data.next_piece.color];
-
-              context.fillRect( ((canvas.width/piece_size)*j) ,((canvas.height/piece_size)*i),canvas.width/piece_size, canvas.height/piece_size);
-              context.strokeStyle="#f9f9f9";
-              context.strokeRect( ((canvas.width/piece_size)*j) ,((canvas.height/piece_size)*i),canvas.width/piece_size, canvas.height/piece_size);
- 
-            }
-        }
-
-
-	     }
     }
 
+    if(data.next_piece){
+      var piece_size = (data.next_piece.shape[0]).length;
+      var canvas = document.getElementById(data.board_key == "+17327305402" ? 'player1_next' : 'player2_next');
+      var context = canvas.getContext('2d');
+
+      canvas.width = canvas.width;
+
+      for(var i = 0; i < piece_size; i++){
+        for(var j = 0; j < piece_size; j++ ) {
+          if (data.next_piece.shape[i][j] != 0){
+            var width = canvas.width/piece_size,
+                height = canvas.height/piece_size;
+
+            context.fillStyle = $scope.colors[data.next_piece.color];
+            context.fillRect(width*j, height*i, width, height);
+            context.strokeStyle="#f9f9f9";
+            context.strokeRect(width*j, height*i, width, height);
+          }
+        }
+      }
     }
-
-    	$scope.drawPieces(data.board_key);
-
-
-
-    });
-
-
-
+  });
 }]);
-
-
-
-  // $scope.drawPieces = function () {
-  //       var canvas = document.getElementById('myCanvas');
-  //       var context = canvas.getContext('2d');
-
- 
-
-  // }
-
